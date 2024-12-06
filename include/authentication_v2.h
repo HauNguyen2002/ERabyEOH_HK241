f5ace0d35c5f0d7016b585e4a05dee644a56e891
HauNguyen2002 <93597645+HauNguyen2002@users.noreply.github.com>
2024-12-06 13:48:12 +0700

    Fix BH1750 setup

---
+++ include/authentication_v2.h
@@ -0,0 +1,109 @@
+// #ifndef AUTHENTICATION_H
+// #define AUTHENTICATION_H
+
+// #include <Arduino.h>
+// #include <freeRTOS/FreeRTOS.h>
+// #include <M5Unified.h>
+// #include "MFRC522_I2C.h"
+// #include "credential_store.h"
+
+// #if CONFIG_FREERTOS_UNICORE
+// static const BaseType_t app_cpu = 0;
+// #else
+// static const BaseType_t app_cpu = 1;
+// #endif
+
+// // USER DEFINES
+// #define MAX_BLOCK_BUFFER 16
+// #define MAX_MSG_BUFFER 100
+// #define LED_ON_BOARD 48
+// #define NUM_INFO_TYPE 3
+// #define BLOCK_PER_SECTOR 4
+// #define MAX_SECTOR 16
+// #define SDA_PIN 11 // Configurable, see typical pin layout above
+// #define SCL_PIN 12 // Configurable, see typical pin layout above
+
+// // GLOBALS
+// static MFRC522 mfrc522(0x28);
+
+// static MFRC522::MIFARE_Key key;
+
+// static byte status;
+
+// enum OpMode
+// {
+//     WAIT,
+//     DUMP,
+//     VALIDATE,
+//     ADD,
+//     CHANGE_PASSWORD,
+//     DELETE
+// };
+
+// enum InfoType
+// {
+//     USERNAME,
+//     PASSWORD,
+//     ID
+// };
+
+// enum WriteMode
+// {
+//     ALL,
+//     ONLY_PASSWORD
+// };
+
+// // CLASSES
+
+// typedef struct RFID_Device
+// {
+//     byte firstBlock;
+//     byte dataBuffer[MAX_BLOCK_BUFFER + 2];
+//     byte byteCount = sizeof(dataBuffer);
+//     byte trailerBuffer[MAX_BLOCK_BUFFER + 2];
+//     byte dataBlockAddr;
+//     byte trailerBlockAddr;
+//     byte blockOffset = 1;
+//     bool isNewSector = true;
+// } RFID_Device;
+
+// typedef struct UserInfo
+// {
+//     byte id;
+//     char username[MAX_BLOCK_BUFFER + 1] = "";
+//     char password[MAX_BLOCK_BUFFER + 1] = "";
+//     byte location[NUM_INFO_TYPE][2];
+
+// } UserInfo;
+
+// typedef struct Door
+// {
+//     bool isOpened = false;
+//     bool isValidated = false;
+// } Door;
+
+// // Forward Declarations
+// bool checkInvertedNibblesMatch(byte *trailerBuffer);
+
+// void printTextData();
+
+// void dumpData(void *parameters);
+
+// bool clearAllDataBlock();
+
+// bool writeUserInfoToRFID(WriteMode write_mode);
+
+// int validateCredentialAccount();
+
+// void doCLI(void *parameters);
+
+// void taskCenter(void *parameters);
+
+// bool validateUser();
+
+// void addNewUser();
+
+// void updateUserPassword();
+// void deleteUser();
+
+// #endif // AUTHENTICATION_H
---
+++ include/global.h
@@ -0,0 +1,33 @@
+// #ifndef GLOBAL_H
+// #define GLOBAL_H
+
+// #define ERA_LOCATION_VN
+// // #define ERA_AUTH_TOKEN "1b2f6d4f-0330-43bb-9ac8-8b31ce65c61f" // Must be on top
+// #define ERA_AUTH_TOKEN "d0ca164a-fefe-49c8-a17d-93e960134a5f" // Must be on top
+
+// #include <Arduino.h>
+// #include <freeRTOS/FreeRTOS.h>
+// #include <nvs_flash.h>
+// #include <nvs.h>
+// #include <ERa.hpp>
+// #include <Widgets/ERaWidgets.hpp>
+// #include "credential_store.h"
+// #include "authentication.h"
+
+// #if CONFIG_FREERTOS_UNICORE
+// static const BaseType_t app_cpu = 0;
+// #else
+// static const BaseType_t app_cpu = 1;
+// #endif
+
+// #define LED_ON_BOARD 48
+// #define SDA_PIN 11 // Configurable, see typical pin layout above
+// #define SCL_PIN 12 // Configurable, see typical pin layout above
+
+// // GLOBALS
+// const char ssid[] = "Guess who?";
+// const char pass[] = "yourmama";
+// // const char ssid[] = "THANHHAU";
+// // const char pass[] = "30110902";
+
+// #endif //GLOBAL_H
